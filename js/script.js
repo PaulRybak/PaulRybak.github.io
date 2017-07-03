31ca82a7f2457193fc6e051fb17a81b6c9644b40
PaulRybak <paulfisher@ukr.net>
2017-07-03 19:07:56 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
-$(function(){
-    
- $('a:eq(0)').click(function(){
-     $('body').animate({
-         scrollTop:$('#section2').offset().top
-     },1000)     
- })
-  $('a:eq(1)').click(function(){
-     $('body').animate({
-         scrollTop:$('#section3').offset().top
-     },1000)     
- })
-   $('a:eq(2)').click(function(){
-     $('body').animate({
-         scrollTop:$('#section4').offset().top
-     },1000)     
- })
-      $('a:eq(3)').click(function(){
-     $('body').animate({
-         scrollTop:$('#section5').offset().top
-     },1000)     
- });
-    $('p').css("opacity","0");
-    
-    $(window).scroll(function(){
-        
-        if($('body').scrollTop()>$('#section2').offset().top-300){
-            $('p:eq(0)').animate({opacity:"i"},800);
-        }
-    })
-      
- })
+var slideIndex=1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+
+	if(n > slides.length){
+		slideIndex = 1;
+	}
+	if(n < 1){
+		slideIndex = slides.length;
+	}
+	for(i=0; i<slides.length; i++){
+		slides[i].style.display = "none";
+	}
+	for(i=0; i < dots.length; i++){
+		dots[i].className = dots[i].className.replace("active","");
+	}
+	 slides[slideIndex-1].style.display = "block";
+	 dots[slideIndex-1].className+= " active";
+}
